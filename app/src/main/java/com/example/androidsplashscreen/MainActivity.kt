f2c1e8db711b10cbbd5a50f6682fdf8793b0d826
Sebastian <pratto.sebastian.ignacio@gmail.com>
2022-07-18 15:36:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/androidsplashscreen/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.androidsplashscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.androidsplashscreen.ui.theme.AndroidSplashScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val mainViewModel: MainViewModel by viewModels()
+
+        installSplashScreen().setKeepVisibleCondition {
+            mainViewModel.isScreenLoading.value!!
+        }
+
+        setContent {
+            AndroidSplashScreenTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    MainContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Android splash screen",
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidSplashScreenTheme {
+        MainContent()
+    }
+}
